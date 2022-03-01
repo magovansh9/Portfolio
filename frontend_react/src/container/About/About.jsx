45cd7028b327a645e4103ea6df23a54e5d19da9f
Vansh Mago <vanshmago9@gmail.com>
2022-03-01 01:53:02 -0500

    Finished About Section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,8 +1,60 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import "./About.scss";
+import { images } from "../../constants";
+
+const abouts = [
+  {
+    title: "Web Development",
+    description: "I am a good web developer",
+    imgUrl: images.about01,
+  },
+  {
+    title: "UI/UX",
+    description: "I am a good UI/UX developer",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Backend Engineering",
+    description: "I am a good backend developer",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Web Animations",
+    description: "I am a good web animator",
+    imgUrl: images.about04,
+  },
+];
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <h2 className='head-text'>
+        I Know That <span>Good Dev</span>
+        <br /> means <span>Good Business</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='p-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default About;
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -27,13 +27,13 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className='p-text'>Hello, I am</p>
-            <h1 className='head-text'>Micael</h1>
+            <h1 className='head-text'>Vansh</h1>
           </div>
         </div>
 
         <div className='tag-cmp app__flex'>
-          <p className='p-text'>Web Developer</p>
-          <p className='p-text'>Freelancer</p>
+          <p className='p-text'>Software Engineer</p>
+          <p className='p-text'>Musician</p>
         </div>
       </div>
     </motion.div>
